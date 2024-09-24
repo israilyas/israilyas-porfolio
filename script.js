ef2da760dbfd3632d82fb09e2b7afc35291b9181
Isra Ilyas <israilyasq@gmail.com>
2024-09-24 17:54:02 +0500

    header completed with responsive screens and toggle for small screens

---
+++ script.js
@@ -0,0 +1,9 @@
+//  Header Toggle 
+
+let  MenuBtn = document.getElementById('menuBtn')
+
+MenuBtn.addEventListener('click', function(e) {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+
+})
