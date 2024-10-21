2c1304f448e433474d0ad2450b8ab77b839b7b8f
ISRA ILYAS <israqureshi138@gmail.com>
2024-10-21 22:58:36 +0500

    ajdusted active link on scroll

--- script.js
+++ script.js
@@ -19,9 +19,25 @@ let typed = new Typed('.auto-input',{
 })
 
 
-// Active link
+// Active link State on Scroll
 
-let  active-link = document.querySelector('')
+let  navLinks = document.querySelectorAll('nav ul li a')
 
+// Get All Sections 
 
+let sections = document.querySelectorAll('section')
+console.log(sections)
 
+window.addEventListener("scroll",  function(){
+    const scrollPos =  window.scrollY;
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active')
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+        }
+    } );
+});
