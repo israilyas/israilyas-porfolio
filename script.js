c9ef64ae1a6928626152e10a80cb4ee55f4f75fd
ISRA ILYAS <israqureshi138@gmail.com>
2024-10-21 20:52:52 +0500

    adjusted home h1 size for smaller screens

--- script.js
+++ script.js
@@ -11,65 +11,17 @@ MenuBtn.addEventListener('click', function(e) {
 // Typing Effect
 
 let typed = new Typed('.auto-input',{
-    strings: ['Front-End Developer!','Freelancer','Python Programmer','Database Administrator','Backlinks Builder'],
+    strings: ['Front-End Developer!','Freelancer','Python Programmer','Backlinks Builder'],
     typeSpeed:100,
-    backSpeed:50,
-    backDelay:2000,
+    backSpeed:70,
+    backDelay:1000,
     loop:true,
 })
 
-// Close Nav Menu when click on any link
-let links = document.querySelectorAll('.nav-link'); // Select all nav links
 
-links.forEach(function(link) {
-    link.addEventListener('click', function(e) {
-        document.querySelector('body').classList.toggle('mobile-nav-active');
-        document.getElementById('menuBtn').classList.toggle('fa-xmark');
-        // Optionally, handle active state
-        links.forEach(l => l.classList.remove('active')); // Remove active class from all links
-        link.classList.add('active'); // Add active class to the clicked link
-    });
-});
+// Active link
 
-
-// ====== Todo == Crreate project Page ======
-
-/// Blur on popup
-
-// let projectLink = document.querySelector('.projectLink');
-
-// projectLink.addEventListener('click',function toggle(){
-
-//     let Blur = document.getElementById('blur');
-//     let projectPage = document.querySelector('.project-page');
-
-//     blur.classList.toggle('active')
-//     projectPage.classList.add('projectPageActive')
-
-// });
-
-// Function to toggle the display of the project page and blur effect
-// function toggle() {
-//     let blur = document.getElementById('blur');
-//     let projectPage = document.querySelector('.project-page');
-
-//     // Toggle the active class for both elements
-//     blur.classList.toggle('active');
-//     projectPage.classList.toggle('active'); // Add or remove active class
-// }
-
-// // Adding event listener to the project link
-// let projectLink = document.querySelector('.projectLink');
-// projectLink.addEventListener('click', toggle);
-
-// // Adding event listener to the close button
-// let closeButton = document.getElementById('project-close-button');
-// closeButton.addEventListener('click', toggle);
-
-
-
-
-// Project page
+let  active-link = document.querySelector('')
 
 
 
